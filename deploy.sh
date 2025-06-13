be03cce0fe7708b25deb1921a08f36abbfc60e73
Ness Bokobza <ness.bokobza@hyperguest.com>
2025-06-13 13:05:20 +0300

    chore: clean up comments in deploy script for clarity

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# 1. Build the Docker image
+# Build the Docker image
 docker build -t bokoness/flask-app:latest src/.
 
-# 2. Deploy to Kubernetes
+# Deploy to Kubernetes
+kubectl apply -f k8s/configmap.yml
 kubectl apply -f k8s/deployment.yml
 kubectl apply -f k8s/service.yml
+kubectl apply -f k8s/hpa.yml
 
-# 3. Wait for pods to be ready (optional, adjust timeout as needed)
+# Wait for pods to be ready (optional, adjust timeout as needed)
 echo "Waiting for pods to be ready..."
 kubectl wait --for=condition=ready pod -l app=flask-app --timeout=120s
 
-# 4. Get the name of a running pod
+# Get the name of a running pod
 POD_NAME=$(kubectl get pods -l app=flask-app -o jsonpath='{.items[0].metadata.name}')
-
-# 5. Apply Horizontal Pod Autoscaler (HPA) configuration
-kubectl apply -f k8s/hpa.yml
 
 # Port-forward the pod to localhost:5000
 echo "Port-forwarding pod $POD_NAME to localhost:5000..."
